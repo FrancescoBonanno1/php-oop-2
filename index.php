aa2c99c0bf20c8556bfeb04b951badcf9b3997a5
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-15 18:32:14 +0100

    oggetti creati e visualizzati in pagina dinamicamente con foreach

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
-require __DIR__ .'/models/data.php';
-
-$generictoy1 = new Giochi_Generici("Pallina Rimbalzina", "Giocattolo Rimbalzante","P.L.U.", "Adatto a tutti i tipi di animali") 
+require __DIR__ . '/metods/data.php';
+$generictoy1 = new Giochi_Generici("Pallina Rimbalzina", "Giocattolo Rimbalzante", "P.L.U.", "Adatto a tutti i tipi di animali");
+$generictoy2 = new Giochi_Generici("Topolino di pezza", "Giocattolo di Tessuto", "Cotone 100%", "Adatto a tutti i tipi di animali");
+$generictoy3 = new Giochi_Generici("Pupazzo che scappa", "Giocattolo con carica a molla", "80% tessuto, plastica 20%", "Adatto a tutti i tipi di Animali");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PetShop</title>
 </head>
+
 <body>
     <div>
-        <?="<h2>Oggetti Generici Per Animali<h2>"?>
+        <?= "<h2>Oggetti Generici Per Animali<h2>" ?>
     </div>
-    <div id="card">
-        <?=$generictoy1-> getgentoy()?>
-    </div>
+
+    <?php
+    $generictoy = [$generictoy1, $generictoy2, $generictoy3];
+    
+    foreach ($generictoy as $toy) {
+        echo '<div id="card">';
+        echo $toy->getgentoy();
+        echo '</div>';
+    }
+    ?>
+
+
 </body>
+
 </html>
--- metods/data.php
+++ metods/data.php
@@ -1,20 +1,21 @@
-<?php class Giochi_Generici {
-public $Name;
-public $Type;
-public $Material;
-static $Suitable;
+<?php class Giochi_Generici
+{
+    public $Name;
+    public $Type;
+    public $Material;
+    public $Suitable;
 
-public function getgentoy(){
-    $result = $this->Name . ", " . $this->Type . ", " .  $this->Material . ", " .  $this->Suitable . ".";
-    return $result;
-}
+    public function getgentoy()
+    {
+        $result = $this->Name . "<br> " . $this->Type . "<br> " .  $this->Material . "<br> " .  $this->Suitable . "<br>";
+        return $result;
+    }
 
-public function __construct($Nome , $Tipo, $Materiale, $Adatto) {
-   $this->Name = $Nome ;
-   $this->Type = $Tipo ;
-   $this->Material = $Materiale ;
-   $this->Suitable = $Adatto ;
-
-
-}
+    public function __construct($Nome, $Tipo, $Materiale, $Adatto)
+    {
+        $this->Name = $Nome;
+        $this->Type = $Tipo;
+        $this->Material = $Materiale;
+        $this->Suitable = $Adatto;
+    }
 }
