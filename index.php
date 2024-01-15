44fd75b12ed5d9703493a9b3c236cf01afe74ff2
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-15 18:52:08 +0100

    aggiunto cibo per gatti

--- index.php
+++ index.php
@@ -4,9 +4,13 @@ $generictoy1 = new Giochi_Generici("Pallina Rimbalzina", "Giocattolo Rimbalzante
 $generictoy2 = new Giochi_Generici("Topolino di pezza", "Giocattolo di Tessuto", "Cotone 100%", "Adatto a tutti i tipi di animali");
 $generictoy3 = new Giochi_Generici("Pupazzo che scappa", "Giocattolo con carica a molla", "80% tessuto, plastica 20%", "Adatto a tutti i tipi di Animali");
 
-$dogf1 =new Cibo_Per_Cani("Polpettine di carne", "Petfood", "60% Manzo, 40% POllo", "Adatto ai cani di taglia Media/Grande");
-$dogf2 =new Cibo_Per_Cani("Bocconcini di pollo", "Friskas", "100% Pollo", "Adatto ai cani di taglia Media/Grande");
-$dogf3 =new Cibo_Per_Cani("Croccantini Junior", "Petxyness", "30% frumento/30% Salmone/40% Manzo", "Adatto ai cuccioli da 3 a 12 mesi");
+$dogf1 = new Cibo_Per_Cani("Polpettine di carne", "Petfood", "60% Manzo, 40% POllo", "Adatto ai cani di taglia Media/Grande");
+$dogf2 = new Cibo_Per_Cani("Bocconcini di pollo", "Friskas", "100% Pollo", "Adatto ai cani di taglia Media/Grande");
+$dogf3 = new Cibo_Per_Cani("Croccantini Junior", "Petxyness", "30% frumento/30% Salmone/40% Manzo", "Adatto ai cuccioli da 3 a 12 mesi");
+
+$catf1 = new Cibo_Per_Gatti("Patè di carne", "Petfood", "60% Manzo, 40% Pollo", "Adatto a gatti adulti (1 anno in su)");
+$catf2 = new Cibo_Per_Gatti("Croccantini Junior", "Friskas", "30% frumento/30% Salmone/40% Manzo", "Adatto ai cuccioli da 3 a 11 mesi");
+$catf3 = new Cibo_Per_Gatti("erba gatta", "Petxyness", "100% Erba gatta", "Adatta per tutti i tipi di gatti");
 ?>
 
 <!DOCTYPE html>
@@ -20,12 +24,12 @@ $dogf3 =new Cibo_Per_Cani("Croccantini Junior", "Petxyness", "30% frumento/30% S
 
 <body>
     <div>
-        <?= "<h2>Oggetti Generici Per Animali<h2>" ?>
+        <?= "<h2>Giochi Generici Per Animali<h2>" ?>
     </div>
 
     <?php
     $generictoy = [$generictoy1, $generictoy2, $generictoy3];
-    
+
     foreach ($generictoy as $toy) {
         echo '<div id="card">';
         echo $toy->getgentoy();
@@ -33,16 +37,30 @@ $dogf3 =new Cibo_Per_Cani("Croccantini Junior", "Petxyness", "30% frumento/30% S
     }
     ?>
 
-<div>
+    <div>
         <?= "<h2>Cibo Per Cani<h2>" ?>
     </div>
 
     <?php
     $DogFood = [$dogf1, $dogf2, $dogf3];
-    
+
     foreach ($DogFood as $food) {
         echo '<div id="card">';
         echo $food->getdogfood();
+        echo '</div>';
+    }
+    ?>
+
+    <div>
+        <?= "<h2>Cibo Per Gatti<h2>" ?>
+    </div>
+
+    <?php
+    $CatFood = [$catf1, $catf2, $catf3];
+
+    foreach ($CatFood as $food) {
+        echo '<div id="card">';
+        echo $food->getcatfood();
         echo '</div>';
     }
     ?>
--- metods/data.php
+++ metods/data.php
@@ -40,3 +40,25 @@
         $this->For = $Adatto;
     }
 }?>
+
+<?php class Cibo_Per_Gatti
+{
+    public $Type;
+    public $Company;
+    public $Ingredients;
+    public $For;
+
+    public function getcatfood()
+    {
+        $result = $this->Type . "<br> " . $this->Company . "<br> " .  $this->Ingredients . "<br> " .  $this->For . "<br>";
+        return $result;
+    }
+
+    public function __construct($Nome, $Marca, $Ingredienti, $Adatto)
+    {
+        $this->Type = $Nome;
+        $this->Company = $Marca;
+        $this->Ingredients = $Ingredienti;
+        $this->For = $Adatto;
+    }
+}?>
